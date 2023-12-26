e4cb2b8b985417043d642059661cf0e803232acb
Bariq Qushoyyi <bariqqushoyyi@gmail.com>
2023-12-26 14:54:40 +0700

    change folder structure

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InternalServicesModule } from './internal-services/internal-services.module';
 
 @Module({
-  imports: [],
+  imports: [InternalServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
